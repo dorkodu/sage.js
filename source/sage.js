fcd79c4ef260b7a2e42296ac4a6b4d4d47ffbfd8
Berk Cambaz <berkcambaz12321@gmail.com>
2021-07-23 21:28:58 +0300

    Mark fetchPolicy as optional parameter

--- source/sage.js
+++ source/sage.js
@@ -3,7 +3,7 @@ export class Sage {
    * 
    * @param {object} options 
    * @param {string} options.url
-   * @param {string} options.fetchPolicy
+   * @param {string} [options.fetchPolicy]
    */
   constructor(options) {
 
