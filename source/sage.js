1bd3a42055b2c7913ed655512c7ef03caf6c5e5b
Berk Cambaz <berkcambaz12321@gmail.com>
2021-07-24 13:50:12 +0300

    Change sage class to constructor function

--- source/sage.js
+++ source/sage.js
@@ -1,18 +1,18 @@
-export class Sage {
+/**
+ * 
+ * @param {object} options 
+ * @param {string} options.url
+ * @param {string} [options.fetchPolicy]
+ */
+export function Sage(options) {
+  const options = options;
+
   /**
    * 
-   * @param {object} options 
-   * @param {string} options.url
-   * @param {string} [options.fetchPolicy]
+   * @param {object} query 
+   * @param {object} [options] 
    */
-  constructor(options) {
-    /**
-     * 
-     * @param {object} query 
-     * @param {object} [options] 
-     */
-    this.want = function (query, options) {
+  this.want = function (query, options) {
 
-    }
   }
 }
