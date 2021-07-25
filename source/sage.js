0359e3f49e67e2494007cd6f824f7d45eba6f3bf
Berk Cambaz <berkcambaz12321@gmail.com>
2021-07-25 12:45:34 +0300

    Add basic fetch functionality and change options to sageOptions to avoid naming collision

--- source/sage.js
+++ source/sage.js
@@ -5,7 +5,7 @@
  * @param {string} [options.fetchPolicy]
  */
 export function Sage(options) {
-  const options = options;
+  const sageOptions = options;
 
   /**
    * 
@@ -13,6 +13,10 @@ export function Sage(options) {
    * @param {object} [options] 
    */
   this.want = function (query, options) {
-
+    return fetch(sageOptions.url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(query)
+    }).then(response => response.json())
   }
 }
